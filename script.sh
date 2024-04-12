d4622b5dd09ab3323af023b0756c424c749cea7b
Nitin <nitinnamdeo456@gmail.com>
2024-04-12 16:38:28 +0530

    change the file path

--- script.sh
+++ script.sh
@@ -4,9 +4,23 @@ FIRST_COMMIT_HASH='c940dd84621df36524518f6ebea4d8646f7bc160'
 
 current_hash=$(git log --oneline -1 --pretty=format:%H)
 
-git diff "${FIRST_COMMIT_HASH}" "${current_hash}"
+gitchanges=$(git diff --name-status "$FIRST_COMMIT_HASH" "$current_hash")
+
+
+echo "Git Changes - - "
+echo "$gitchanges"
+
+
+while IFS= read -r line; do
+	modificationType=$(echo "$line" | cut -d' ' -f1)
+	apiFilePath=$(echo "$line" | cut -d' ' -f2)
+
+	if [ "${apiFilePath%%/*}" != "APIs" ]; then
+        echo "Ignoring file: $apiFilePath"
+        continue
+    fi
 
 
 
 
-
+done < <(echo "$gitchanges")
